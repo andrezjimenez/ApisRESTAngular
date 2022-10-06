558cb8e3d652753508243d21fe91b957be5b3470
AndrezJimenez <andru456@hotmail.com>
2022-10-05 23:08:15 -0500

    login services 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service'
 import { UsersService } from './services/users.service'
 import { Auth } from './models/auth.model';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,11 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  token = '';
+
   constructor(
      private authService : AuthService,
-     private UsersService : UsersService,
+     private usersService : UsersService,
   ){  }
 
 
@@ -27,9 +30,9 @@ export class AppComponent {
   }
 
   createUser(){
-    this.UsersService.create({
+    this.usersService.create({
       name: 'Andres',
-      email: 'andru456@hotmail.com',
+      email: '1@hotmail.com',
       password: '123456'
     })
     .subscribe(rta => {
@@ -38,10 +41,24 @@ export class AppComponent {
   }
 
   login(){
-    this.authService.login('andru456@hotmail.com','123456')
+    this.authService.login('1@hotmail.com','123456')
     .subscribe(rta => {
       console.log(rta);
-    })
+      this.token = rta.access_token;
+      console.log(this.token);
+    });
   }
 
+  getProfile(){
+    this.authService.profile(this.token)
+    .subscribe(profile =>{
+      console.log(profile);
+    })
+  }
+  getProfile2(){
+    this.authService.getProfile(this.token)
+    .subscribe(profile =>{
+    console.log(profile);
+  })
+}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams,HttpErrorResponse,HttpStatusCode } from '@angular/common/http';
+import { HttpClient, HttpParams,HttpErrorResponse,HttpStatusCode,HttpHeaders } from '@angular/common/http';
 import { Auth } from './../models/auth.model';
+import { User } from './../models/user.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +18,25 @@ export class AuthService {
     login(email: string, password : string){
       return this.http.post<Auth>(`${this.apiURL}/login`,{email, password});
     }
+    
     // --token: string
-    profile(){
-      return this.http.get(`${this.apiURL}/profile`);
+    profile(token : string){
+    //   const headers = new HttpHeaders();
+    //   headers.set('authorization',`Bearer ${token}`)
+      return this.http.get<User>(`${this.apiURL}/profile`,{
+        headers: {
+          Authorization: `Bearer ${token}`,
+          // 'Content-type': 'application/json'
+        }
+      });
     }
+
+    getProfile(token: string) {
+      let headers = new HttpHeaders();
+      headers = headers.set('Authorization', `Bearer ${token}`);
+      headers = headers.set('Content-type', 'application/json');
+      return this.http.get<User>(`${this.apiURL}/profile`, { headers });
+    }
+
 }
  
